41d84a3d662e55ca6e065de5037257cdb61f932f
Romario <Romario@LAPTOP-AKUHMEPM>
2022-07-04 15:17:49 -0400

    Creando clase B

---
+++ ProyectoFinal/src/logico/B.java
@@ -0,0 +1,19 @@
+package logico;
+
+public class B {
+	private int cantidad;
+
+	public B(int cantidad) {
+		super();
+		this.cantidad = cantidad;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
