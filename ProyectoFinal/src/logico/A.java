e892c76a30fbf621abb03d03800b205723318970
Wolflags <Wolflags@DESKTOP-MTT7Q9S>
2022-07-04 15:16:21 -0400

    Creando clase A

---
+++ ProyectoFinal/src/logico/A.java
@@ -0,0 +1,20 @@
+package logico;
+
+public class A {
+	int num;
+
+	public A(int num) {
+		super();
+		this.num = num;
+	}
+
+	public int getN1() {
+		return num;
+	}
+
+	public void setN1(int n1) {
+		this.num = num;
+	}
+	
+
+}
