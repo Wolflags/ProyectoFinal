7e4a2551a7cafff37f9ddb36c2c133f0f713b426
Romario <Romario@LAPTOP-AKUHMEPM>
2022-07-31 23:45:48 -0400

    Arreglando ListadoClientes

--- ProyectoFinal/src/visual/ListadoClientes.java
+++ ProyectoFinal/src/visual/ListadoClientes.java
@@ -175,7 +175,7 @@ public class ListadoClientes extends JDialog {
 		for (Persona cliente : clientes) {
 			row[0] = cliente.getCedula();
 			row[1] = ""+cliente.getNombre()+" "+cliente.getApellido();
-			row[3] = Integer.toString(((Cliente) cliente).getMisPlanes().size());
+			row[3] = Integer.valueOf(((Cliente) cliente).getMisPlanes().size()).toString();
 			model.addRow(row);
 		}
 	}
